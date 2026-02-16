909bd42946575d9ff511650714dd21d4020f088c
FeathersRe <e0969003@u.nus.edu>
2026-02-16 20:47:31 +0800

    Add fetch_one_tfds_sample function for single sample retrieval

--- AI/tensorflow-model/data/inspect_data.py
+++ AI/tensorflow-model/data/inspect_data.py
@@ -3,7 +3,7 @@ import tensorflow_datasets as tfds
 import os 
 import numpy as np
 
-def inspect_speech_command_data(data_dir=None,dataset_name='speech_commands', split='test'):
+def inspect_speech_command_data(dataset_name='speech_commands', split='test', data_dir=None):
     if data_dir is None:
         script_dir = os.path.dirname(os.path.abspath(__file__))
         data_dir = os.path.join(script_dir, 'tensorflow_datasets')
@@ -33,6 +33,26 @@ def inspect_speech_command_data(data_dir=None,dataset_name='speech_commands', sp
 
     #Training data will now make use of these commands: "go", "on", "stop", "up", "down", "_silence_" and "_unknown_"
     target_commands = np.array(['go', 'on', 'stop', 'up', 'down', '_silence_', '_unknown_'])
+
+def fetch_one_tfds_sample(dataset_name="speech_commands", split="test", data_dir=None):
+    if data_dir is None:
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        data_dir = os.path.join(script_dir, 'tensorflow_datasets')
+
+    dataset, info = tfds.load(
+        dataset_name, 
+        split=split, 
+        with_info=True, 
+        data_dir=data_dir)
+    label_names = info.features['label'].names
+    
+    for ex in dataset.take(1):
+        audio = ex['audio'].numpy()
+        label_idx = ex['label'].numpy()
+        label_name = label_names[label_idx]
+        print(f"Audio shape: {audio.shape}, Label index: {label_idx}, Label name: {label_name}")
+
+        return audio, label_idx, label_name
 
 if __name__ == "__main__":
     inspect_speech_command_data()
